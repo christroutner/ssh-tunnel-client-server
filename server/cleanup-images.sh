30205896271e9db7d460faf24e584bfc57754fc7
Chris Troutner <chris.troutner@gmail.com>
2025-11-30 14:31:28 -0800

    Creating new cleanup-imags.sh script

--- server/cleanup-images.sh
+++ server/cleanup-images.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-# Remove all untagged docker images (dangling images).
-# Using Docker's built-in filter for dangling images
-dangling_images=$(docker images --filter "dangling=true" -q)
-
-if [ -z "$dangling_images" ]; then
-    echo "No dangling images found."
-    exit 0
-fi
-
-docker rmi $dangling_images
-
+docker system prune
