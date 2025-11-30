eead278c36f442d69bff6b68335a78b923131ad8
Chris Troutner <chris.troutner@gmail.com>
2025-11-30 09:20:35 -0800

    Updating cleanup-images.sh script for modern Docker

--- server/cleanup-images.sh
+++ server/cleanup-images.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-# Remove all untagged docker images.
-docker rmi $(docker images | grep "^<none>" | awk '{print $3}')
+# Remove all untagged docker images (dangling images).
+# Using Docker's built-in filter for dangling images
+dangling_images=$(docker images --filter "dangling=true" -q)
+
+if [ -z "$dangling_images" ]; then
+    echo "No dangling images found."
+    exit 0
+fi
+
+docker rmi $dangling_images
 
